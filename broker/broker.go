5310e0511b92c749c19c6ca1bea9037144bc20fb
tervicke <ameywpatil@gmail.com>
2025-04-21 14:17:07 +0530

    added extraction method from the packet

--- broker/broker.go
+++ broker/broker.go
@@ -3,14 +3,31 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 const addr string = "localhost:8080";
 var Activeconnection []net.Conn;
 
 func handleConnection(curCon net.Conn){
+	defer curCon.Close()
 	Activeconnection = append(Activeconnection,curCon);
 	fmt.Printf("connection accepted \n Total no of clients are now %d\n",len(Activeconnection));
+	buf :=	make([]byte,2049);
+	packet_size,err := curCon.Read(buf);
+	if err != nil{
+		fmt.Println("Error reading the buffer");
+		return;
+	}
+	if packet_size < 2049{
+		fmt.Println("invalid packet recieved");
+	}
+	packet_type := int(buf[0]);
+	fmt.Println("Packet Type: ",packet_type);
+	topic := strings.Trim(string(buf[1:1025]),"\x00");
+	fmt.Println("Topic:",topic);
+	payload := strings.Trim(string(buf[1025:2049]),"\x00");
+	fmt.Println("Paylod:",payload);
 }
 
 func main() {
