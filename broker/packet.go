0d816675ba9f4fc30d4645b3713d6af1220fecce
tervicke <ameywpatil@gmail.com>
2025-04-22 12:13:09 +0530

    made packet to Packet

--- broker/packet.go
+++ broker/packet.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-type packet struct{
+type Packet struct{
 	Type uint8
 	Topic [1024]byte
 	Payload [1024]byte
 }
 
-func newPacket(packetbuffer [2049]byte) packet{
-	newpacket := packet{
+func newPacket(packetbuffer [2049]byte) Packet{
+	newpacket := Packet{
 		Type: uint8(packetbuffer[0]),
 		Topic : [1024]byte(packetbuffer[1:1025]),
 		Payload :[1024]byte(packetbuffer[1025:2049]),
@@ -19,7 +19,7 @@ func newPacket(packetbuffer [2049]byte) packet{
 	return newpacket;
 }
 
-func (p *packet) Print() {
+func (p *Packet) Print() {
 	fmt.Printf("Type: %d\n", p.Type)
 	fmt.Printf("Topic: %s\n", p.Topic)
 	fmt.Printf("Payload: %s\n", p.Payload)
