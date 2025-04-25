5cd0e494b6dab81bcb9902087a7d56376af625b4
tervicke <ameywpatil@gmail.com>
2025-04-25 17:57:15 +0530

    made agent send req a gorountine

--- agent/agentpublish.go
+++ agent/agentpublish.go
@@ -2,9 +2,9 @@ package agent
 
 import (
 	"errors"
-	"fmt"
 	"strings"
 	"time"
+	"fmt"
 )
 
 func (a* agent) Publish(topic string , message string) error{
@@ -12,23 +12,24 @@ func (a* agent) Publish(topic string , message string) error{
 	buf[0] = 4; 
 	copy(buf[1:2025] , []byte(topic))
 	copy(buf[1025:2049] , []byte(message))
-	a.conn.Write(buf[:])
-	fmt.Println("message sent from agent")
+	go func() {
+        _, err := a.conn.Write(buf[:])
+        if err != nil {
+						return errors.New("Error occured recieved wrong ack")
+            return
+        }
+    }()
+	//	a.conn.Write(buf[:])
 	select{
 	case ack := <- a.ackchan:
 		recieved_topic := strings.Trim(ack.Topic,"\x00")
 		recieved_message := strings.Trim(ack.Payload,"\x00")
-		fmt.Println("------------------------------")
-		fmt.Println(topic , len(topic) , )
-		fmt.Println(ack.Topic , len(recieved_topic) )
-		fmt.Println("------------------------------")
 		if recieved_topic == topic && recieved_message == message{
-			fmt.Println("recived the ack!!!!!!!!!!")
+			return nil
 		}else{
-			fmt.Println("hello ????")
+			return errors.New("Error occured recieved wrong ack")
 		}
 	case <-time.After(3 * time.Second):
 		return errors.New("Did not recieve ack")
 	}
-	return nil
 }
