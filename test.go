2cbceda385a5501680160c0b869a192c675a8cc6
tervicke <ameywpatil@gmail.com>
2025-04-25 16:45:14 +0530

    added test.go with the new publish method

--- test.go
+++ test.go
@@ -3,22 +3,29 @@ package main
 import (
 	"Tolstoy/agent"
 	"fmt"
-	"time"
+	"flag"
+	"os"
 )
 
 func main(){
-	addr := "localhost:8080"
-	agent,err := agent.NewAgent(addr)
+	addr := flag.String("addr","","The adress to connect to")
+	topic := flag.String("topic","","The topic to send messages to")
+	flag.Parse()
+	if *addr == "" || *topic == "" {
+		fmt.Println("Error: both addr and topic are required.")
+		flag.Usage()
+		os.Exit(1)   
+	}
+	agent,err := agent.NewAgent(*addr)
 	if err != nil{
 		panic(err)
 	}
-	agent.BeginListening()
-	time.Sleep(1 * time.Second)
-	agent.StopListening()
-	time.Sleep(2 * time.Second)
-	fmt.Println("doing it all over again")
-	time.Sleep(1 * time.Second)
-	agent.BeginListening()
-	time.Sleep(1 * time.Second)
-	agent.Terminate()
+	var message string;
+	for {
+		fmt.Scan(&message)
+		if message == "exit" {
+			os.Exit(0)
+		}
+		agent.Publish(*topic, message)
+	}
 }
