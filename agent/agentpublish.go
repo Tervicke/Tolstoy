fb9dd097d46046085a06cd95aec5159a0b9dd501
tervicke <ameywpatil@gmail.com>
2025-05-03 11:34:07 +0530

    remove the goroutine when writing the publish message from agent

--- agent/agentpublish.go
+++ agent/agentpublish.go
@@ -9,21 +9,20 @@ import (
 
 func (a* agent) Publish(topic string , message string) error{
 	packet := makepacket(4,topic,message) //make a packet and then write it
-	go func() {
-        _, err := a.conn.Write(packet.tobytes())
-        if err != nil {
-						fmt.Println("Error occured recieved wrong ack")
-						return
-        }
-    }()
-	//	a.conn.Write(buf[:])
+	_, err := a.conn.Write(packet.tobytes())
+	if err != nil {
+		fmt.Println("Error",err)
+	}
 	select{
 	case ack := <- a.ackchan:
 		recieved_topic := strings.Trim(ack.Topic,"\x00")
 		recieved_message := strings.Trim(ack.Payload,"\x00")
 		if recieved_topic == topic && recieved_message == message{
+			fmt.Println("recieved ack")
 			return nil
 		}else{
+			fmt.Printf("expected - %s - recieved - %s\n",topic ,recieved_topic)
+			fmt.Printf("expected - %s - recieved - %s\n",message,recieved_message)
 			return errors.New("Error occured recieved wrong ack")
 		}
 	case <-time.After(3 * time.Second):
