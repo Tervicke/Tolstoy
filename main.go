75b885fe53fa3ba863c7ca4889e6f0f66397d42f
tervicke <ameywpatil@gmail.com>
2025-06-16 15:59:15 +0530

    added version flag

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"github.com/Tervicke/Tolstoy/broker"
 	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Tervicke/Tolstoy/broker"
 )
+const VERSION = "v0.4.0";
+
 func main(){
 	config_path := flag.String("config" , "config.yaml" ,"The path to the config.yaml file \n by default it searches the current directory")                              
+	showVersion := flag.Bool("version" , false , "print the version and exit")
 	flag.Parse()
+	if(*showVersion){
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
 	broker.StartServer(*config_path)
 }
